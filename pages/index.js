7bd229cb490aeb06ef2a7ad34c3bdb5736729688
crycetruly <crycetruly@gmail.com>
2019-09-04 16:43:22 +0300

    ch(tests): set up Cypress for e2e testing

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import Head from 'next/head'
 
-const Home = () => ( <
-  div >
-  <
-  Head >
-  <
-  title > Home < /title> <
-  /Head> <
-  p > Welcome to App < /p>
-
-  <
-  /div>
+const Home = () => ( 
+<div>
+  <Head>
+  <title> Home </title> 
+  </Head> 
+  <p> Welcome to App </p>
+  </div>
 )
 
 export default Home
