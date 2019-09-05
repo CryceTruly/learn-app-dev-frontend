cd6844121cd508cd5365e950e4ee545f3e02a757
crycetruly <crycetruly@gmail.com>
2019-09-05 13:00:09 +0300

    ch(test): Cleanup default Cypress Examples

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -2,7 +2,7 @@ const baseUrl='http://localhost:3030/'
 
 describe('Home',()=>{
     it('should be valid',()=>{
-    cy.visit('http://google.com');
-    cy.contains('Google');
+    cy.visit('https://cocky-lamport-d9ae8a.netlify.com/');
+    cy.contains('Welcome');
     });
 });
