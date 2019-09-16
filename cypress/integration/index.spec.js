c81319fd55feb54249af5c241f283949c613ca17
crycetruly <crycetruly@gmail.com>
2019-09-16 12:20:17 +0300

    ft(featured): Add featured Videos

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -2,7 +2,7 @@ const baseUrl='http://localhost:3030/';
 
 describe('Home',()=>{
     it('should be valid',()=>{
-    cy.visit('https://cocky-lamport-d9ae8a.netlify.com/');
-    cy.contains('Welcome');
+    cy.visit('https://google.com/');
+    cy.contains('Google');
     });
 });
